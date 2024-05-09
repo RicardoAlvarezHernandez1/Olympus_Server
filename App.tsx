273785c2b0ab05429d92a92e2ee1e5a7bfcf78f3
rica07b <rica07b@gmail.com>
2024-05-09 16:38:35 +0100

    feat : configuration and content download

--- App.tsx
+++ App.tsx
@@ -1,20 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
+import React from "react";
+import { NavigationContainer, Theme } from "@react-navigation/native";
+import CustomDrawer from "./components/CustomDrawer";
+import appColors from "./assets/styles/appColors";
+import UserProvider from "./providers/AdminProvider";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <UserProvider>
+        <NavigationContainer>
+          <CustomDrawer></CustomDrawer>
+        </NavigationContainer>
+      </UserProvider>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
   },
 });
